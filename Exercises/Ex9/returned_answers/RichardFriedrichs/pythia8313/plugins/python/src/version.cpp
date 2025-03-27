291245626c671217ebc51005edf37b4a8958dad3
Richard Friedrichs <richard.fredrichs@hotmail.com>
2025-03-27 15:01:36 +0000

    Again, cleaned up first half of hw9

--- Exercises/Ex9/returned_answers/RichardFriedrichs/pythia8313/plugins/python/src/version.cpp
+++
@@ -1,8 +0,0 @@
-#include <Python.h>
-#include <iostream>
-int main() {
-  std::string include("2.9.2");
-  if (PY_MAJOR_VERSION >= 3 && PY_MINOR_VERSION > 5) include = "2.10.4"; 
-  std::cout << include << "\n";
-  return 0;
-}
